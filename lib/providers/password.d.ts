e256569071e11bf70e9758ae1300a3b894847f54
Richard Fox <prime@ahref.co.uk>
2016-07-29 22:56:59 +0100

    Update typings

--- lib/providers/password.d.ts
+++ lib/providers/password.d.ts
@@ -5,11 +5,20 @@ import Client = require("../client");
 import { BeamRequest } from "../../defs/request";
 import { BeamUserSelf } from "../../defs/user";
 
-declare class PasswordProvider {
+import Provider = require("./provider");
+
+declare class PasswordProvider extends Provider {
+  static CSRF_TOKEN_LOCATION: String;
+  static INVALID_CSRF_CODE: Number;
   /**
    * The cookie jar for the provider.
    */
   jar: request.CookieJar;
+  /**
+   * Storage of the CSRF Token as given in the response from the server
+   * @type {String}
+   */
+  csrfToken: String;
   /**
    * The options for the client.
    */
